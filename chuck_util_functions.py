422731585255e000aae2a374100b880899c2a330
Dealga McArdle <zeffii@users.noreply.github.com>
2013-12-06 21:20:47 +0100

    Update chuck_util_functions.py
    modularized, and now accepts me.dir(), me.dir(0), me.dir(-1)..etc.
    seems to work under my ideal circumstances.

--- chuck_util_functions.py
+++ chuck_util_functions.py
@@ -3,16 +3,12 @@ import subprocess
 import os
 import re
 
-def open_wav_file(line_under_cursor, path, wtype):
-    """
-    looks at the current line and converts from
+def get_full_path_to_file(levels, file_found, path):
+    path_strings = [s for s in file_found.split("/") if s]
+    dest_path = os.sep.join(path.split(os.sep)[:levels])
+    return os.path.join(dest_path,*path_strings)
 
-    this:     me.dir(-1) + "/audio/hihat_02.wav" => string p;
-    to this:  "/..fullpath ../audio/hihat_02.wav"
-
-    accepts:  me.dir()  , me.dir(0),  me.dir(-1) , me.dir(-n)
-    """
-
+def get_levels(line_under_cursor):
     # if empty between parenthesis quicly set to 0, else let regex find it.
     if "me.dir()" in line_under_cursor:
         levels = 0
@@ -22,21 +18,31 @@ def open_wav_file(line_under_cursor, path, wtype):
             m = re.search(dir_pattern, line_under_cursor)
             levels = int(m.groups(0)[0])
         except:
-            print("not the expected form, see open_wav_file doc_string")
-            return
+            print("malformed me.dir(), remove spaces..")
+            return None
+    
+    return levels
+
+def open_wav_file(line_under_cursor, path, wtype):
+    """
+    looks at the current line and converts from
+    this:     me.dir(-1) + "/audio/hihat_02.wav" => string p;
+    to this:  "/..fullpath ../audio/hihat_02.wav"
+    accepts:  me.dir()  , me.dir(0),  me.dir(-1) , me.dir(-n)
+    """
+
+    # if empty between parenthesis quicly set to 0, else let regex find it.
+    levels = get_levels(line_under_cursor)
+    if levels == None:
+        print("please let us know in what context you get this error.")
+        return 
 
     # find sample_name
     try:
         pattern = "\"(.*" + wtype + ")\""
         m = re.search(pattern, line_under_cursor)
         file_found = m.groups(0)[0]
-
-        sublime.status_message(file_found)
-        path_strings = [s for s in file_found.split("/") if s]
-        print(path, path_strings)
-
-        dest_path = os.sep.join(path.split(os.sep)[:levels])
-        full_wav_path = os.path.join(dest_path,*path_strings)
+        full_wav_path = get_full_path_to_file(levels, file_found, path)
 
         settings = sublime.load_settings("ChucK.sublime-settings")
         wave_editor = settings.get("wave_editor")
@@ -49,15 +55,18 @@ def open_wav_file(line_under_cursor, path, wtype):
     pass
 
 def open_chuck_file(line_under_cursor, path):
+    right_side = line_under_cursor.strip()
 
-    right_side = line_under_cursor.split("me.dir()")[1].strip() 
+    levels = get_levels(line_under_cursor)
+    if levels == None:
+        print("please let us know in what context you get this error.")
+        return 
 
     try:
         m = re.search("\"\W(\w*.ck)", right_side)
         file_found = m.groups(0)[0]
-        full_path_to_file = os.path.join(path, file_found)
-        sublime.status_message(full_path_to_file)
-    
+        full_path_to_file = get_full_path_to_file(levels, file_found, path)
+
         window = sublime.active_window()           
         window.open_file(full_path_to_file)
 
@@ -79,10 +88,9 @@ class ChuckOpener(sublime_plugin.TextCommand):
         path = os.path.dirname(file_path)
 
         if "Machine.add(" in line_under_cursor:
-            # consistent with miniAudicle i'll force the use of
-            # me.dir() to be present in the string to mean current 
-            # file path. (later dir(-1) will be supported )
-            if not "me.dir()" in line_under_cursor:
+            # consistent with miniAudicle forcing the use of
+            # some form of me.dir() can use  empty, or 0 or -1, -2 etc..
+            if not "me.dir(" in line_under_cursor:
                 sublime.status_message("Machine.add() must contain me.dir()")
             else:
                 open_chuck_file(line_under_cursor, path)
@@ -96,8 +104,7 @@ class ChuckOpener(sublime_plugin.TextCommand):
             found_types = [(s in line_under_cursor) for s in sample_types]
 
             for idx, sample_type in enumerate(found_types):
-                # stop at the first, and send this line off to the open_wav_file
-                # function
+                # stop immediately, go with first find.
                 if sample_type:
                     open_wav_file(line_under_cursor, path, sample_types[idx])
 
