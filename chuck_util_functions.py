e1340ed49a266144d775746388398c848a2edc43
Dealga McArdle <zeffii@users.noreply.github.com>
2013-12-07 19:35:54 +0100

    Update chuck_util_functions.py
    refactored.

--- chuck_util_functions.py
+++ chuck_util_functions.py
@@ -10,6 +10,7 @@ def get_full_path_to_file(levels, file_found, path):
 
 def get_levels(line_under_cursor):
     # if empty between parenthesis quicly set to 0, else let regex find it.
+    levels = None
     if "me.dir()" in line_under_cursor:
         levels = 0
     else:
@@ -19,60 +20,57 @@ def get_levels(line_under_cursor):
             levels = int(m.groups(0)[0])
         except:
             print("malformed me.dir(), remove spaces..")
-            return None
-    
+
     return levels
 
-def open_wav_file(line_under_cursor, path, wtype):
-    """
-    looks at the current line and converts from
-    this:     me.dir(-1) + "/audio/hihat_02.wav" => string p;
-    to this:  "/..fullpath ../audio/hihat_02.wav"
-    accepts:  me.dir()  , me.dir(0),  me.dir(-1) , me.dir(-n)
-    """
-
-    # if empty between parenthesis quicly set to 0, else let regex find it.
-    levels = get_levels(line_under_cursor)
-    if levels == None:
-        print("please let us know in what context you get this error.")
-        return 
-
-    # find sample_name
+def open_file(line_under_cursor, path, levels, file_type):
     try:
-        pattern = "\"(.*" + wtype + ")\""
+        pattern = "\"(.*" + file_type + ")\""
         m = re.search(pattern, line_under_cursor)
         file_found = m.groups(0)[0]
-        full_wav_path = get_full_path_to_file(levels, file_found, path)
-
-        settings = sublime.load_settings("ChucK.sublime-settings")
-        wave_editor = settings.get("wave_editor")
-        subprocess.call("{0} {1}".format(wave_editor, full_wav_path))
+        full_path = get_full_path_to_file(levels, file_found, path)
 
     except:
-        print("failure wave!")
-        pass
+        print("failure! ", file_type)
+        return
 
-    pass
+    finally:
+        if file_type == ".ck":
+            window = sublime.active_window()           
+            window.open_file(full_path)
 
-def open_chuck_file(line_under_cursor, path):
-    right_side = line_under_cursor.strip()
+        # has to be a sound file
+        else:
+            settings = sublime.load_settings("ChucK.sublime-settings")
+            wave_editor = settings.get("wave_editor")
+            subprocess.call("{0} {1}".format(wave_editor, full_path))
 
+
+def check_file(line_under_cursor, path):
+    """ figures out if the line contains a file in the expected notation."""
+
+    # does it have a path? if not, ditch return no ifs ands or buts
     levels = get_levels(line_under_cursor)
     if levels == None:
         print("please let us know in what context you get this error.")
         return 
 
-    try:
-        m = re.search("\"\W(\w*.ck)", right_side)
-        file_found = m.groups(0)[0]
-        full_path_to_file = get_full_path_to_file(levels, file_found, path)
+    # is it a sound?
+    sample_types = [".wav", ".aiff", ".mat"]  # extend if needed
+    found_types = [(s in line_under_cursor) for s in sample_types]
 
-        window = sublime.active_window()           
-        window.open_file(full_path_to_file)
+    # enters here if the filetype is recognized.
+    if any(found_types):
+        for i, sample_type in enumerate(found_types):
+            if sample_type:
+                open_file(line_under_cursor, path, levels, sample_types[i])
+        return
 
-    except:
-        print("failure!")
-        pass
+    # is it a .ck? 
+    if ".ck" in line_under_cursor:
+        open_file(line_under_cursor, path, levels, ".ck")
+
+
 
 
 class ChuckOpener(sublime_plugin.TextCommand):
@@ -87,26 +85,11 @@ class ChuckOpener(sublime_plugin.TextCommand):
         file_path = view.file_name()
         path = os.path.dirname(file_path)
 
-        if "Machine.add(" in line_under_cursor:
-            # consistent with miniAudicle forcing the use of
-            # some form of me.dir() can use  empty, or 0 or -1, -2 etc..
-            if not "me.dir(" in line_under_cursor:
-                sublime.status_message("Machine.add() must contain me.dir()")
-            else:
-                open_chuck_file(line_under_cursor, path)
+        if not "me.dir(" in line_under_cursor:
+            sublime.status_message("line must contain some form of me.dir()")
+        else:
+            check_file(line_under_cursor, path)
 
-        # purely convenience here, stick with how miniAudicle expects this stuff
-        # or you run into problems and it won't find files.  
-        # normal form:   me.dir(-1) + "/audio/hihat_02.wav" => string p;  
-
-        if "me.dir(" in line_under_cursor:
-            sample_types = [".wav", ".aiff", ".mat"]  # extend if needed
-            found_types = [(s in line_under_cursor) for s in sample_types]
-
-            for idx, sample_type in enumerate(found_types):
-                # stop immediately, go with first find.
-                if sample_type:
-                    open_wav_file(line_under_cursor, path, sample_types[idx])
 
     def enabled(self):
         view = self.view
