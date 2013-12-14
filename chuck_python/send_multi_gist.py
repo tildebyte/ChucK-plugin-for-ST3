13a3f1da6d5f626764182d7205777634debb5194
Dealga McArdle <zeffii@users.noreply.github.com>
2013-12-14 18:15:38 +0100

    Create send_multi_gist.py
    oh yeah baby!

---
+++ chuck_python/send_multi_gist.py
@@ -0,0 +1,33 @@
+import json
+from urllib.request import urlopen
+ 
+def upload(gist_files_dict, project_name, public_switch):
+  
+    gist_post_data = {  'description': project_name, 
+                        'public': public_switch,
+                        'files': gist_files_dict
+                    }
+ 
+    json_post_data = json.dumps(gist_post_data).encode('utf-8')
+ 
+    def get_gist_url(found_json):
+        wfile = json.JSONDecoder()
+        wjson = wfile.decode(found_json)
+        gist_url = 'https://gist.github.com/' + wjson['id']
+ 
+        import webbrowser
+        print(gist_url)
+        webbrowser.open(gist_url)
+        # or just copy url to clipboard?
+ 
+    def upload_gist():
+        print('sending')
+        url = 'https://api.github.com/gists'
+        json_to_parse = urlopen(url, data=json_post_data)
+        
+        print('received response from server')
+        found_json = json_to_parse.readall().decode()
+        get_gist_url(found_json)
+ 
+    upload_gist()
+ 
